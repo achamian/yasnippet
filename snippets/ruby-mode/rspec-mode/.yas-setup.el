067a504979256b3f419f0808b71dcbb0584f564c
Niranjan Paranjape <niranjan@c42.in>
2011-12-10 00:36:11 +0530

    adding rspec mode snippets from TextMate bundle

---
+++ snippets/ruby-mode/rspec-mode/.yas-setup.el
@@ -0,0 +1,27 @@
+;; .yas-setup.el for rspec-mode
+;; 
+;; --**--
+;; Automatically generated code, do not edit this part
+;; 
+;; Translated menu
+;; 
+
+
+;; Unknown substitutions
+;; 
+;; Substitutions for: content
+;; 
+;; 
+
+;; Substitutions for: condition
+;; 
+;; # as in twice.yasnippet
+;;                                                                                            =yyas> (yas/unknown)
+;; 
+;; 
+
+;; Substitutions for: binding
+;; 
+;; 
+
+;; .yas-setup.el for rspec-mode ends here
